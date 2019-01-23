7ac1333440eb8edf2195b6286056dd255c4f9288
neronix17 <39186138+neronix17@users.noreply.github.com>
2019-01-23 05:47:26 +0000

    Initial Commit

---
+++ Source/O21Toolbox/O21Toolbox/O21ToolboxMod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+using Harmony;
+
+
+namespace O21Toolbox
+{
+    [StaticConstructorOnStartup]
+    public class O21ToolboxMod : Mod
+    {
+        public static O21ToolboxSettings settings;
+
+        public Vector2 scrollPos = new Vector2();
+
+        public O21ToolboxMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<O21ToolboxSettings>();
+        }
+
+        public override string SettingsCategory() => "Outpost 21 Toolbox";
+    }
+}
---
+++ Source/O21Toolbox/O21Toolbox/O21ToolboxSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace O21Toolbox
+{
+    public class O21ToolboxSettings : ModSettings
+    {
+        public static O21ToolboxSettings Instance;
+
+        public O21ToolboxSettings()
+        {
+            O21ToolboxSettings.Instance = this;
+        }
+
+        public override void ExposeData()
+        {
+
+        }
+
+        public void ResetToDefault()
+        {
+
+        }
+    }
+}
