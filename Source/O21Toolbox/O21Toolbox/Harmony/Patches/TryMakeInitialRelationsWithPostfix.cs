dd26b563ea6430e1543190ef291dbbb75a2b24d1
neronix17 <39186138+neronix17@users.noreply.github.com>
2019-03-08 14:49:51 +0000

    Craptop Commit

---
+++ Source/O21Toolbox/O21Toolbox/Harmony/Patches/TryMakeInitialRelationsWithPostfix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+using Harmony;
+
+using O21Toolbox.Alliances;
+
+namespace O21Toolbox.Harmony
+{
+    
+}
