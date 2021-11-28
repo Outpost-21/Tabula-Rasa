d7647185dff99c4ec0fdba346967d6f42b0f2e39
neronix17 <39186138+neronix17@users.noreply.github.com>
2021-11-28 14:29:39 +0000

    2.1.9

--- 1.3/Source/O21Toolbox/O21Toolbox/AutoHeal/HealUtility.cs
+++ 1.3/Source/O21Toolbox/O21Toolbox/AutoHeal/HealUtility.cs
@@ -49,6 +49,38 @@ namespace O21Toolbox.AutoHeal
             }
         }
 
+        public static bool CanSealWounds(Pawn pawn)
+        {
+            IEnumerable<Hediff> enumerable = from hd in pawn.health.hediffSet.hediffs
+                                             where hd.TendableNow()
+                                             select hd;
+            if (enumerable != null)
+            {
+                List<Hediff> list = enumerable.ToList();
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i] != null && !list[i].def.makesSickThought && !list[i].def.chronic)
+                    {
+                        if (list[i].def.hediffClass == typeof(Hediff_MissingPart))
+                        {
+                            return false;
+                        }
+                        else if (!list[i].IsPermanent() && list[i].def.everCurableByItem)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static void TrySealWounds(Pawn patient)
+        {
+            TrySealWounds(patient, new List<HediffDef>());
+        }
+
         public static void TryCureInfections(Pawn pawn, List<HediffDef> hediffList, List<HediffDef> explicitRemovals)
         {
             IEnumerable<Hediff> enumerable = from hd in pawn.health.hediffSet.hediffs
@@ -109,6 +141,46 @@ namespace O21Toolbox.AutoHeal
                         }
                     }
                 }
+            }
+        }
+
+        public static void TendAdditional(Pawn doctor, Pawn patient)
+        {
+            if (doctor != null && doctor.Faction == Faction.OfPlayer && patient.Faction != doctor.Faction && !patient.IsPrisoner && patient.Faction != null)
+            {
+                patient.mindState.timesGuestTendedToByPlayer++;
+            }
+            if (doctor != null && doctor.Faction == Faction.OfPlayer && patient.Faction != doctor.Faction && !patient.IsPrisoner && patient.Faction != null)
+            {
+                patient.mindState.timesGuestTendedToByPlayer++;
+            }
+            if (doctor != null && doctor.RaceProps.Humanlike && patient.RaceProps.Animal && patient.RaceProps.playerCanChangeMaster && RelationsUtility.TryDevelopBondRelation(doctor, patient, 0.004f) && doctor.Faction != null && doctor.Faction != patient.Faction)
+            {
+                InteractionWorker_RecruitAttempt.DoRecruit(doctor, patient, false);
+            }
+            patient.records.Increment(RecordDefOf.TimesTendedTo);
+            if (doctor != null)
+            {
+                doctor.records.Increment(RecordDefOf.TimesTendedOther);
+            }
+            if (doctor == patient && !doctor.Dead)
+            {
+                doctor.mindState.Notify_SelfTended();
+            }
+            if (ModsConfig.IdeologyActive && doctor != null && doctor.Ideo != null)
+            {
+                Precept_Role role = doctor.Ideo.GetRole(doctor);
+                if (role != null && role.def.roleEffects != null)
+                {
+                    foreach (RoleEffect roleEffect in role.def.roleEffects)
+                    {
+                        roleEffect.Notify_Tended(doctor, patient);
+                    }
+                }
+            }
+            if (doctor != null && doctor.Faction == Faction.OfPlayer && doctor != patient)
+            {
+                QuestUtility.SendQuestTargetSignals(patient.questTags, "PlayerTended", patient.Named("SUBJECT"));
             }
         }
     }
