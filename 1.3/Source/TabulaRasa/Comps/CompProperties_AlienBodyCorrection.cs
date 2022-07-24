4fa6708aad801a88f74d7fc3b2daddeb9feb5f01
neronix17 <39186138+neronix17@users.noreply.github.com>
2022-07-24 17:58:55 +0100

    Steam Version

---
+++ 1.3/Source/TabulaRasa/Comps/CompProperties_AlienBodyCorrection.cs
@@ -0,0 +1,23 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace TabulaRasa
+{
+    public class CompProperties_AlienBodyCorrection : CompProperties
+    {
+        public CompProperties_AlienBodyCorrection()
+        {
+            this.compClass = typeof(Comp_AlienBodyCorrection);
+        }
+
+        public List<BodyTypeDef> maleBodyTypes = new List<BodyTypeDef>();
+
+        public List<BodyTypeDef> femaleBodyTypes = new List<BodyTypeDef>();
+    }
+}
---
+++ 1.3/Source/TabulaRasa/Comps/Comp_IdeoIconOverlay.cs
@@ -0,0 +1,106 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace TabulaRasa
+{
+    public class Comp_IdeoIconOverlay : ThingComp
+    {
+        public CompProperties_IdeoIconOverlay Props => (CompProperties_IdeoIconOverlay)props;
+
+        private Texture2D iconTexture;
+
+        private Color ideoColor = Color.white;
+
+        private Material Graphic
+        {
+            get
+            {
+                if (iconTexture == null)
+                {
+                    iconTexture = Find.FactionManager.OfPlayer.ideos.PrimaryIdeo.Icon;
+                }
+                return MaterialPool.MatFrom(new MaterialRequest(iconTexture));
+            }
+        }
+
+        private bool ShouldDraw
+        {
+            get
+            {
+                if (parent.Rotation == Rot4.South)
+                {
+                    return Props.showSouth;
+                }
+                if (parent.Rotation == Rot4.North)
+                {
+                    return Props.showNorth;
+                }
+                if (parent.Rotation == Rot4.East)
+                {
+                    return Props.showEast;
+                }
+                if (parent.Rotation == Rot4.West)
+                {
+                    return Props.showWest;
+                }
+                return false;
+            }
+        }
+
+        private Vector3 CurrentOffset
+        {
+            get
+            {
+                if (parent.Rotation == Rot4.South)
+                {
+                    return Props.offsetSouth;
+                }
+                if (parent.Rotation == Rot4.North)
+                {
+                    return Props.offsetNorth;
+                }
+                if (parent.Rotation == Rot4.East)
+                {
+                    return Props.offsetEast;
+                }
+                if (parent.Rotation == Rot4.West)
+                {
+                    return Props.offsetWest;
+                }
+                return new Vector3(0, 0, 0);
+            }
+        }
+
+        private Color GetIdeoColor => Find.FactionManager.OfPlayer.ideos.PrimaryIdeo.Color;
+
+        public override void CompTickRare()
+        {
+            base.CompTickRare();
+
+            iconTexture = null;
+            ideoColor = GetIdeoColor;
+        }
+
+        public override void PostDraw()
+        {
+            base.PostDraw();
+            if (ModsConfig.IdeologyActive && ShouldDraw)
+            {
+                MaterialPropertyBlock propertyBlock = new MaterialPropertyBlock();
+                if (parent.Stuff != null && parent.Stuff.stuffProps.color != null)
+                {
+                    propertyBlock.SetColor(ShaderPropertyIDs.Color, ideoColor);
+                }
+                Matrix4x4 matrix = default(Matrix4x4);
+                matrix.SetTRS(parent.DrawPos + CurrentOffset, Quaternion.AngleAxis(0, Vector3.up), new Vector3(Props.drawSize.x, 1, Props.drawSize.y));
+                Graphics.DrawMesh(MeshPool.plane10, matrix, Graphic, 0, null, 0, propertyBlock);
+            }
+        }
+    }
+}
