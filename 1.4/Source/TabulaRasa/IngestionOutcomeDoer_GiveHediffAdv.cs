9715f460a04e97af15aab3fb75a5e8fd2a252f92
neronix17 <39186138+neronix17@users.noreply.github.com>
2023-03-21 07:16:42 +0000

    1.0.8479

---
+++ 1.4/Source/TabulaRasa/IngestionOutcomeDoer_GiveHediffAdv.cs
@@ -0,0 +1,51 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace TabulaRasa
+{
+    public class IngestionOutcomeDoer_GiveHediffAdv : IngestionOutcomeDoer
+    {
+        public List<AdditionalHediffEntry> hediffDefs = new List<AdditionalHediffEntry>();
+
+        public bool randomChosen = false;
+
+        public override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
+        {
+            if (randomChosen)
+            {
+                TryAddHediffFromEntry(pawn, hediffDefs.RandomElementByWeight((AdditionalHediffEntry x) => x.weight));
+            }
+            else
+            {
+                foreach (AdditionalHediffEntry entry in hediffDefs)
+                {
+                    TryAddHediffFromEntry(pawn, entry);
+                }
+            }
+        }
+
+        public void TryAddHediffFromEntry(Pawn pawn, AdditionalHediffEntry entry)
+        {
+            Hediff hediff = pawn.health.hediffSet.GetFirstHediffOfDef(entry.hediff);
+            if (hediff == null)
+            {
+                Hediff newHediff = HediffMaker.MakeHediff(entry.hediff, pawn);
+                newHediff.Severity = entry.severityRange.RandomInRange;
+                pawn.health.AddHediff(newHediff);
+            }
+            else
+            {
+                if(hediff.Severity < entry.severityRange.min)
+                {
+                    hediff.Severity = entry.severityRange.RandomInRange;
+                }
+            }
+        }
+    }
+}
