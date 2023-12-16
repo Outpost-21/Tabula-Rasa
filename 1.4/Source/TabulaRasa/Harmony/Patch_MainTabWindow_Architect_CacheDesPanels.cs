f01d8ed7719c7fd6eb6eb5b0ba2470e43d35dbc5
neronix17 <39186138+neronix17@users.noreply.github.com>
2023-12-16 16:49:39 +0000

    1.0.8750

---
+++ 1.4/Source/TabulaRasa/Harmony/Patch_MainTabWindow_Architect_CacheDesPanels.cs
@@ -0,0 +1,34 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+using HarmonyLib;
+
+namespace TabulaRasa
+{
+    [HarmonyPatch(typeof(MainTabWindow_Architect), "CacheDesPanels")]
+    public static class Patch_MainTabWindow_Architect_CacheDesPanels
+    {
+        [HarmonyPostfix]
+        public static void Postfix(MainTabWindow_Architect __instance)
+        {
+            List<ArchitectCategoryTab> removals = new List<ArchitectCategoryTab>();
+            foreach(ArchitectCategoryTab tab in __instance.desPanelsCached)
+            {
+                if (tab.def.HasModExtension<DefModExt_HideArchitectTab>())
+                {
+                    removals.Add(tab);
+                }
+            }
+            foreach(ArchitectCategoryTab tab in removals)
+            {
+                __instance.desPanelsCached.Remove(tab);
+            }
+        }
+    }
+}
