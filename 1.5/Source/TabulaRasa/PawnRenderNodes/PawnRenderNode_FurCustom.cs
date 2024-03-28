e114aa30149afe1891fb1ab57c6e0cbcec6d68cc
neronix17 <39186138+neronix17@users.noreply.github.com>
2024-03-28 21:26:21 +0000

    1.5.8850

---
+++ 1.5/Source/TabulaRasa/PawnRenderNodes/PawnRenderNode_FurCustom.cs
@@ -0,0 +1,52 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace TabulaRasa
+{
+    public class PawnRenderNode_FurCustom : PawnRenderNode
+	{
+		public override Shader DefaultShader => ShaderDatabase.CutoutSkinOverlay;
+
+		public PawnRenderNode_FurCustom(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree)
+			: base(pawn, props, tree)
+		{
+		}
+
+		public override Graphic GraphicFor(Pawn pawn)
+		{
+			if (!ModLister.CheckBiotech("Fur"))
+			{
+				return null;
+			}
+			if (pawn.story?.furDef == null)
+			{
+				return null;
+            }
+			Color furColor;
+			switch (props.colorType)
+            {
+				case PawnRenderNodeProperties.AttachmentColorType.Custom:
+					furColor = props.color ?? pawn.story.hairColor;
+					break;
+				case PawnRenderNodeProperties.AttachmentColorType.Skin:
+					furColor = pawn.story.SkinColor;
+					break;
+				default:
+					furColor = pawn.story.HairColor;
+					break;
+            }
+            return GraphicDatabase.Get<Graphic_Multi>(pawn.story?.furDef.GetFurBodyGraphicPath(pawn), ShaderFor(pawn), Vector2.one, furColor);
+		}
+
+        public override Color ColorFor(Pawn pawn)
+        {
+            return base.ColorFor(pawn);
+        }
+    }
+}
