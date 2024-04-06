9302c5bcd03e3e5b87e3963db05cb7ee4fb72bd6
neronix17 <39186138+neronix17@users.noreply.github.com>
2024-04-06 23:44:56 +0100

    1.5.8862

---
+++ 1.5/Source/TabulaRasa/Harmony/Patch_PawnRenderUtility_DrawEquipmentAiming.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+using HarmonyLib;
+
+namespace TabulaRasa
+{
+    [HarmonyPatch(typeof(PawnRenderUtility), "DrawEquipmentAiming")]
+    public static class Patch_PawnRenderUtility_DrawEquipmentAiming
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(Thing eq, Vector3 drawLoc, float aimAngle)
+        {
+            if(eq != null && eq.def.HasModExtension<DefModExt_InvisibleWeapon>())
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
