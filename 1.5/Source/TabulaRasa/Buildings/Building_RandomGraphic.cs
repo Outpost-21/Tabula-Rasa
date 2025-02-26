91af4ed383127f64a8fb23859865fe06013595b4
Neronix17 <39186138+neronix17@users.noreply.github.com>
2025-02-26 02:38:19 +0000

    1.5.9180

---
+++ 1.5/Source/TabulaRasa/Buildings/Building_RandomGraphic.cs
@@ -0,0 +1,22 @@
+﻿
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace TabulaRasa
+{
+    public class Building_RandomGraphic : Building
+    {
+
+        public override void PostMake()
+        {
+            base.PostMake();
+            Graphic_Random rg = (base.Graphic as Graphic_Random) ?? ((base.Graphic as Graphic_Linked).subGraphic as Graphic_Random);
+            this.overrideGraphicIndex = new int?(Rand.RangeInclusive(0, rg.SubGraphicsCount));
+        }
+    }
+}
