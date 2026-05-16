5d05622cfc68ad9ad24f5af2fdfaebdf23304495
Neronix17 <39186138+neronix17@users.noreply.github.com>
2026-05-16 22:14:04 +0100

    PawnRenderNode_ApparelColor
    Simply pulls apparel colour for pawn render nodes, for apparel using render nodes.

---
+++ 1.6/Source/TabulaRasa/PawnRenderNodes/PawnRenderNode_ApparelColor.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace TabulaRasa
+{
+    public class PawnRenderNode_ApparelColor : PawnRenderNode
+    {
+        public PawnRenderNode_ApparelColor(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree) : base(pawn, props, tree)
+        {
+            // Required to function.
+        }
+
+        public override Color ColorFor(Pawn pawn)
+        {
+            return apparel.DrawColor;
+        }
+    }
+}
